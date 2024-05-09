05c95e1548b772ece0f59a19bb7a9467ca5e197f
Tommy J Burke <thatgingerdude@gmail.com>
2024-05-09 20:56:10 +0100

    fixed id

--- src/App.jsx
+++ src/App.jsx
@@ -82,6 +82,7 @@ export default function App() {
          </div>
          {selectedFriend && (
             <FormSplitBill
+               key={selectedFriend.id}
                selectedFriend={selectedFriend}
                onSplitBill={handleSplitBill}
             />
